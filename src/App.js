308e372743112d668f098266ee91b1d267a9d670
Abolfazl Nasibi <abolfazlnasibi2024@gmail.com>
2025-03-02 22:46:58 +0330

    completed header texts and background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar/navbar'
+import Header from './components/Header/header'
+import CompanyInfo from './components/Company-info/company-info'
 
 function App() {
   return (
     <div className="App">
        <Navbar></Navbar>
+       <Header></Header>
+       <CompanyInfo></CompanyInfo>
     </div>
   );
 }
---
+++ src/components/Header/header.js
@@ -0,0 +1,14 @@
+import './header.css'
+
+function Header(){
+    return <header>
+        {/* top */}
+        <div>
+        <span>راه حل های قدرتمند دیجیتال با</span>
+        <span>Kasuka</span>
+        <span>ما تیمی از بازاریابان با استعداد دیجیتال هستیم</span>
+        </div>
+    </header>
+}
+
+export default Header
