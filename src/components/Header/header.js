cfc135ed26cfd1b176220630299945d6e1bf28c7
Abolfazl Nasibi <abolfazlnasibi2024@gmail.com>
2025-03-03 00:17:50 +0330

    header complited

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,12 +1,21 @@
 import './header.css'
+import HBox from './HeaderBox/header-box'
 
 function Header(){
     return <header>
         {/* top */}
-        <div>
+        <div className='hTexts'>
         <span>راه حل های قدرتمند دیجیتال با</span>
         <span>Kasuka</span>
         <span>ما تیمی از بازاریابان با استعداد دیجیتال هستیم</span>
+        </div>
+
+        <div className="hBoxes">
+            <HBox title='اطلاعات ذخیره شده'></HBox>
+            <HBox title='عملکرد های پایانی'></HBox>
+            <HBox title='نمونه کار ها'></HBox>
+            <HBox title='نمودار کیفی'></HBox>
+            <HBox title='متن نمایشی'></HBox>
         </div>
     </header>
 }
